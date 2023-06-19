041e259ece7edb48d34816721096207099766a1f
Niklas Bönisch <niklas.boenisch@gmail.com>
2023-06-19 13:46:00 +0200

    Initial

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,19 @@
+<script lang='ts'>
+	// The ordering of these imports is critical to your app working properly
+	import '@skeletonlabs/skeleton/themes/theme-crimson.css';
+	// If you have source.organizeImports set to true in VSCode, then it will auto change this ordering
+	import '@skeletonlabs/skeleton/styles/skeleton.css';
+	// Most of your app wide CSS should be put in this file
+	import '../app.postcss';
+
+	import { AppBar } from '@skeletonlabs/skeleton';
+	import { LightSwitch } from '@skeletonlabs/skeleton';
+</script>
+
+<AppBar gridColumns="grid-cols-3" slotDefault="place-self-center" slotTrail="place-content-end">
+	<svelte:fragment slot="lead">(icon)</svelte:fragment>
+	Bahn Bingo
+	<svelte:fragment slot="trail"><LightSwitch /></svelte:fragment>
+</AppBar>
+
+<slot />
---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	// import Grid from
+	let reasons = []
+
+	for (let index = 0; index < 25; index++) {
+		reasons.push({id: index, text: `Reason ${index}`})
+	}
+
+</script>
+
+<div class="container h-full mx-auto flex justify-center items-center">
+	<div class="grid grid-cols-5 grid-rows-5 h-full w-full mx-auto flex justify-center">
+		{#each reasons as {id, text}, index(id)}
+			<div class="card p-4 flex justify-center">text</div>
+		{/each}
+	</div>
+</div>
